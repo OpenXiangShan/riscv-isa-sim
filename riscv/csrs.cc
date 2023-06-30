b3e5ef49010c65bcf9aa461dafa794cb891a96c4
Yinan Xu <xuyinan@ict.ac.cn>
2023-06-30 15:18:26 +0800

    Update misa write mask for rocket-chip

--- riscv/csrs.cc
+++ riscv/csrs.cc
@@ -682,7 +682,17 @@ bool sstatus_csr_t::enabled(const reg_t which) {
 misa_csr_t::misa_csr_t(processor_t* const proc, const reg_t addr, const reg_t max_isa):
   basic_csr_t(proc, addr, max_isa),
   max_isa(max_isa),
-  write_mask(max_isa & (0  // allow MABFDQCHV bits in MISA to be modified
+#if defined(CPU_ROCKET_CHIP)
+  write_mask(max_isa & (0  // allow MAFDQCHV bits in MISA to be modified
+                        | (1L << ('M' - 'A'))
+                        | (1L << ('A' - 'A'))
+                        | (1L << ('F' - 'A'))
+                        | (1L << ('D' - 'A'))
+                        | (1L << ('C' - 'A'))
+                        | (1L << ('V' - 'A'))
+                        )
+#else
+  write_mask(max_isa & (0  // allow MAFDQCHV bits in MISA to be modified
                         | (1L << ('M' - 'A'))
                         | (1L << ('A' - 'A'))
                         | (1L << ('B' - 'A'))
@@ -693,6 +703,7 @@ misa_csr_t::misa_csr_t(processor_t* const proc, const reg_t addr, const reg_t ma
                         | (1L << ('H' - 'A'))
                         | (1L << ('V' - 'A'))
                         )
+#endif
              ) {
 }
 
