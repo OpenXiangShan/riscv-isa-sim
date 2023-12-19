909db847f6c54cf54e6e797c0df50ea2e39048da
Kamimiao <1317379456@qq.com>
2023-12-19 15:39:33 +0800

    fix run vector spec and fix repeat interrupts under difftest

--- difftest/difftest.cc
+++ difftest/difftest.cc
@@ -102,7 +102,7 @@ void DifftestRef::get_regs(diff_context_t *ctx) {
     ctx->vr[i]._64[1] = vReg_Val1;
   }
   /***************************************************************************************************/
-  ctx->vstart     = vstate.vstart->read();
+  ctx->vstart     = 0;//vstate.vstart->read();
   ctx->vxsat      = vstate.vxsat->read();
   ctx->vxrm       = vstate.vxrm->read();
   ctx->vcsr       = state->csrmap[CSR_VCSR]->read();
@@ -150,7 +150,8 @@ void DifftestRef::set_regs(diff_context_t *ctx, bool on_demand) {
     state->satp->write(ctx->satp);
   }
   if (!on_demand || state->mip->read() != ctx->mip) {
-    state->mip->write(ctx->mip);
+    //state->mip->write(ctx->mip);
+    //printf("not cp mip\n");
   }
   if (!on_demand || state->mie->read() != ctx->mie) {
     state->mie->write(ctx->mie);
