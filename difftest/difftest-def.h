88268446d8dc1c01d5acec4b9e2d9136467d6a29
xuzefan <ceba_robot@outlook.com>
2023-12-31 14:05:20 +0800

    RVH: Support RVH difftest * Add CSRs to difftest for H-extension. * fix problem of accessing s-CSRs when V=1.
    Co-authored-by: Xiaokun Pei <993676483@qq.com>

--- difftest/difftest-def.h
+++ difftest/difftest-def.h
@@ -15,6 +15,7 @@
 
 #if defined(CPU_XIANGSHAN)
 #define CONFIG_DIFF_DEBUG_MODE
+// #define CONFIG_DIFF_RVH  // Default off
 // #define CONFIG_DIFF_RVV  // Default off
 #endif
 
@@ -25,7 +26,9 @@
 #define CONFIG_FLASH_SIZE      0x1000UL
 #define CONFIG_PMP_NUM         0
 #elif defined(CPU_XIANGSHAN)
-    #if defined(CONFIG_DIFF_RVV)
+    #if defined(CONFIG_DIFF_RVH)
+    #define CONFIG_DIFF_ISA_STRING "RV64IMAFDCH_zba_zbb_zbc_zbs_zbkb_zbkc_zbkx_zknd_zkne_zknh_zksed_zksh_svinval"
+    #elif defined(CONFIG_DIFF_RVV)
     #define CONFIG_DIFF_ISA_STRING "RV64IMAFDCV_zba_zbb_zbc_zbs_zbkb_zbkc_zbkx_zknd_zkne_zknh_zksed_zksh_svinval"
     #else
     #define CONFIG_DIFF_ISA_STRING "RV64IMAFDC_zba_zbb_zbc_zbs_zbkb_zbkc_zbkx_zknd_zkne_zknh_zksed_zksh_svinval"
